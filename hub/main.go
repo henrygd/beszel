06e4dd10e02345e9418fcbe6c4257a70ccd3fdf9
Henry Dollman <hank@henrygd.me>
2024-07-23 22:41:33 -0400

    0.0.1-alpha.9

--- hub/main.go
+++ hub/main.go
@@ -30,7 +30,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-var Version = "0.0.1-alpha.8"
+var Version = "0.0.1-alpha.9"
 
 var app *pocketbase.PocketBase
 var serverConnections = make(map[string]*Server)
