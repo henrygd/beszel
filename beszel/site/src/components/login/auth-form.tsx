48ed4abc025b5061cf649227056b5c847e10ba4c
Alexander Mnich <56564725+a-mnich@users.noreply.github.com>
2025-01-17 11:46:59 +0000

    remove lowercase of placeholders in auth-form

--- beszel/site/src/components/login/auth-form.tsx
+++ beszel/site/src/components/login/auth-form.tsx
@@ -154,7 +154,7 @@ export function UserAuthForm({
 									type="password"
 									autoComplete="current-password"
 									disabled={isLoading || isOauthLoading}
-									className="ps-9 placeholder:lowercase"
+									className="ps-9 placeholder"
 								/>
 								{errors?.password && <p className="px-1 text-xs text-red-600">{errors.password}</p>}
 							</div>
@@ -172,7 +172,7 @@ export function UserAuthForm({
 										type="password"
 										autoComplete="current-password"
 										disabled={isLoading || isOauthLoading}
-										className="ps-9 placeholder:lowercase"
+										className="ps-9 placeholder"
 									/>
 									{errors?.passwordConfirm && <p className="px-1 text-xs text-red-600">{errors.passwordConfirm}</p>}
 								</div>
