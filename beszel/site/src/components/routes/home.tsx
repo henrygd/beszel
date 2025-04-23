02d5f71e88fe2b97f303dce41288d46a36ecd7f4
Dao Khoi Nguyen <nguyendk.on@gmail.com>
2025-04-23 11:16:28 +0700

    refactor(home): remove GitHub links and update version display
    The GitHub links were removed to simplify the UI, and the version display was updated to reflect the correct application name, CMonitor.

--- beszel/site/src/components/routes/home.tsx
+++ beszel/site/src/components/routes/home.tsx
@@ -63,21 +63,10 @@ export const Home = memo(() => {
 				</Suspense>
 
 				<div className="flex gap-1.5 justify-end items-center pe-3 sm:pe-6 mt-3.5 text-xs opacity-80">
-					<a
-						href="https://github.com/henrygd/beszel"
-						target="_blank"
-						className="flex items-center gap-0.5 text-muted-foreground hover:text-foreground duration-75"
-					>
-						<GithubIcon className="h-3 w-3" /> GitHub
-					</a>
 					<Separator orientation="vertical" className="h-2.5 bg-muted-foreground opacity-70" />
-					<a
-						href="https://github.com/henrygd/beszel/releases"
-						target="_blank"
-						className="text-muted-foreground hover:text-foreground duration-75"
-					>
-						Beszel {globalThis.BESZEL.HUB_VERSION}
-					</a>
+					<p className="text-muted-foreground hover:text-foreground duration-75">
+						CMonitor {globalThis.BESZEL.HUB_VERSION}
+					</p>
 				</div>
 			</>
 		),
