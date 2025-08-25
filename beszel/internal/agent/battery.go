53a87fab9287c57c46a39ad7a439a86c8ebed51b
henrygd <hank@henrygd.me>
2025-08-24 21:46:30 -0400

    sum all batteries for battery stats

--- beszel/internal/agent/battery.go
+++ beszel/internal/agent/battery.go
@@ -4,15 +4,21 @@ import "github.com/distatus/battery"
 
 // getBatteryStats returns the current battery percent and charge state
 func getBatteryStats() (batteryPercent uint8, batteryState uint8, err error) {
-	bat, err := battery.Get(0)
+	batteries, err := battery.GetAll()
 	if err != nil {
 		return batteryPercent, batteryState, err
 	}
-	full := bat.Design
-	if full == 0 {
-		full = bat.Full
+	batteriesTotalCapacity := float64(0)
+	batteriesTotalCharge := float64(0)
+	for _, bat := range batteries {
+		full := bat.Design
+		if full == 0 {
+			full = bat.Full
+		}
+		batteriesTotalCapacity += full
+		batteriesTotalCharge += bat.Current
 	}
-	batteryPercent = uint8(bat.Current / full * 100)
-	batteryState = uint8(bat.State.Raw)
+	batteryPercent = uint8(batteriesTotalCharge / batteriesTotalCapacity * 100)
+	batteryState = uint8(batteries[0].State.Raw)
 	return batteryPercent, batteryState, nil
 }
