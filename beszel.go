29b182fd7b34efdec1a854117ad1557f79459878
henrygd <hank@henrygd.me>
2025-09-24 18:08:54 -0400

    0.12.11 release :)

--- beszel.go
+++ beszel.go
@@ -6,7 +6,7 @@ import "github.com/blang/semver"
 
 const (
 	// Version is the current version of the application.
-	Version = "0.12.10"
+	Version = "0.12.11"
 	// AppName is the name of the application.
 	AppName = "beszel"
 )
