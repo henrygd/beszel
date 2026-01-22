23c495814523b588c71ca34b7b8e2923be2bd634
henrygd <hank@henrygd.me>
2026-01-21 19:09:57 -0500

    increase `smartctl --scan` timeout to 10 seconds (#1465)

--- agent/smart.go
+++ agent/smart.go
@@ -171,7 +171,7 @@ func (sm *SmartManager) ScanDevices(force bool) error {
 		configuredDevices = parsedDevices
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	cmd := exec.CommandContext(ctx, sm.binPath, "--scan", "-j")
