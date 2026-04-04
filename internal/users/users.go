ed7312ddc44c5e9cf84388cca9fe047aa5710006
svenvg93 <svenvanginkel@icloud.com>
2026-04-04 12:19:42 +0200

    bug: use toLowerCase for mail

--- internal/users/users.go
+++ internal/users/users.go
@@ -4,6 +4,7 @@ package users
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/henrygd/beszel/internal/migrations"
 
@@ -83,7 +84,8 @@ func (um *UserManager) CreateFirstUser(e *core.RequestEvent) error {
 
 	collection, _ := um.app.FindCollectionByNameOrId("users")
 	user := core.NewRecord(collection)
-	user.SetEmail(data.Email)
+	email := strings.ToLower(data.Email)
+	user.SetEmail(email)
 	user.SetPassword(data.Password)
 	user.Set("role", "admin")
 	user.Set("verified", true)
@@ -93,7 +95,7 @@ func (um *UserManager) CreateFirstUser(e *core.RequestEvent) error {
 	// create superuser using the email of the first user
 	collection, _ = um.app.FindCollectionByNameOrId(core.CollectionNameSuperusers)
 	adminUser := core.NewRecord(collection)
-	adminUser.SetEmail(data.Email)
+	adminUser.SetEmail(email)
 	adminUser.SetPassword(data.Password)
 	if err := um.app.Save(adminUser); err != nil {
 		return e.JSON(http.StatusInternalServerError, map[string]string{"err": err.Error()})
