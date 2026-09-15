875d1e436475b739b4f09937a975e1e60667dfb7
henrygd <hank@henrygd.me>
2026-09-14 21:07:15 -0400

    bump dev version

--- beszel.go
+++ beszel.go
@@ -6,7 +6,7 @@ import "github.com/blang/semver"
 
 const (
 	// Version is the current version of the application.
-	Version = "0.19.0"
+	Version = "0.20.0-dev"
 	// AppName is the name of the application.
 	AppName = "beszel"
 )
