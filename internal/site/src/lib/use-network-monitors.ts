60c5b61e23d1c969ec380e6cfdf413d509e25145
sveng93 <svenvanginkel@icloud.com>
2026-09-21 21:29:37 +0200

    add count to monitor fields

--- internal/site/src/lib/use-network-monitors.ts
+++ internal/site/src/lib/use-network-monitors.ts
@@ -74,7 +74,7 @@ async function fetchMonitorStats(
 }
 
 const NETWORK_MONITOR_FIELDS =
-	"id,system,target,protocol,port,interval,res,resMin1h,resMax1h,resAvg1h,loss1h,enabled,updated"
+	"id,system,target,protocol,port,interval,count,res,resMin1h,resMax1h,resAvg1h,loss1h,enabled,updated"
 
 interface UseNetworkMonitorsProps {
 	systemId?: string
