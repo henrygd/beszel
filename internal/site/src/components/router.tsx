fb4e93f09811f7efc442c4a1218736287767aa4a
henrygd <hank@henrygd.me>
2026-09-26 12:26:39 -0400

    refactor(site): use globalThis to access BESZEL for base path

--- internal/site/src/components/router.tsx
+++ internal/site/src/components/router.tsx
@@ -15,7 +15,7 @@ const routes = {
  * The base path of the application.
  * This is used to prepend the base path to all routes.
  */
-export const basePath = BESZEL?.BASE_PATH || ""
+export const basePath = globalThis.BESZEL?.BASE_PATH || ""
 
 /**
  * Prepends the base path to the given path.
